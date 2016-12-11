1504a9978cdc853c9f7d17e635869de761fe5e8a
omarchaeopteryx <omaralimalik@gmail.com>
2016-12-10 17:19:56 -0800

    Add js vendor requirements per guidelines

---
+++ app/controllers/graph_controller.rb
@@ -0,0 +1,13 @@
+class GraphController < ApplicationController
+  def index  # Gives us the template that will hold the graph.
+  end
+
+ # Gives us json data we can use to build the graph. Note that we have to go into the routes controller and tell it to not use the defaults as erb.
+  def data
+    respond_to do |format|
+      format.json {
+        render :json => [1,2,3,4,5, 6, 7]
+      }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  root 'home#index'
+
+  get 'graph/index'
+  get 'graph/data', :defaults => { :format => 'json' } # Must do this!
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
