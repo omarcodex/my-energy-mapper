13c21bce94184db3a23bdc61da1c34bfd438a46d
omarchaeopteryx <omaralimalik@gmail.com>
2016-12-12 08:30:49 -0800

    Fix database model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   get 'graph/index'
   # get 'graph/data', :defaults => { :format => 'json' } # Must do this!
-
+  resources :states
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
