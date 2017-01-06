51c90a9f25330ddbd5a913cb751eb9e63cedee0a
omarchaeopteryx <omaralimalik@gmail.com>
2017-01-05 22:04:12 -0800

    Update background, fix root location

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'graph/index'
+  root 'graph#index'
   # get 'graph/data', :defaults => { :format => 'json' } # Must do this!
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
